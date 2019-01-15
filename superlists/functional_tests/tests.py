4dddc1104148e82ff25299ebc7d6f827f142e5ad
zplotimkun <timkun_@hotmail.com>
2019-01-14 23:03:46 -0800

    "let functional_tests be app, use LiveServerTestCase"

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from django.test import LiveServerTestCase
 
 
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome(
-            '/home/user/Desktop/python/djg_test/chromedriver')
+            '/home/Djg/Djg/superlists/chromedriver')
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get('self.live_server_url')
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
