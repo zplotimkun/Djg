42ab25359dee3df73eeb4b9e2529fbb8b8e381f0
zplotimkun <eddie@5xruby.twtimkun_@hotmail.com>
2019-01-16 14:20:53 +0800

    Adjust model and let projects be associated with different lists

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     if request.method == 'POST':
@@ -13,5 +13,6 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
