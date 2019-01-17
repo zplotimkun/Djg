ea709c42a59b9e9b0700c78f045f6a4e1e8054dd
zplotimkun <eddie@5xruby.twtimkun_@hotmail.com>
2019-01-17 16:17:57 +0800

    first steps of FT for layout + styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -68,5 +68,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    
+
         self.fail('Finish the test!')
+
+    def test_layout_and_styling(self):
+            self.browser.get(self.live_server_url)
+            self.browser.set_window_size(1024, 768)
+
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=5
+            )
+
+            inputbox.send_keys('testing\n')
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=5
+            )
 
